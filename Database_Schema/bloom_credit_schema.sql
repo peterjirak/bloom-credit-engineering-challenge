e665c9c1a874b779b2b6499b510d825e0164ec61
Peter Eldritch <peter.jirak@gmail.com>
2021-05-23 23:04:42 -0700

    Modified bloom_credit_schema.sql: Added the table consumer_credit_metrics.

--- Database_Schema/bloom_credit_schema.sql
+++ Database_Schema/bloom_credit_schema.sql
@@ -21,3 +21,13 @@ CREATE TABLE IF NOT EXISTS credit_tag(
 -- DROP TABLE IF EXISTS credit_tag;
 
 ALTER TABLE credit_tag ADD CONSTRAINT `credit_tag_uniq_name` UNIQUE (`name`);
+
+CREATE TABLE IF NOT EXISTS consumer_credit_metrics(
+    consumer_uuid BINARY(16) NOT NULL,
+    credit_tag_id NOT NULL,
+    score INTEGER(9) SIGNED NOT NULL,
+    CONSTRAINT consumer_credit_metrics_pk PRIMARY KEY(consumer_uuid, credit_tag_id)
+) ENGINE=InnoDb;
+
+ALTER TABLE consumer_credit_metrics ADD CONSTRAINT `consumer_credit_metrics_fk_consmr_uuid` FOREIGN KEY (consumer_uuid) REFERENCES consumer(uuid);
+ALTER TABLE consumer_credit_metrics ADD CONSTRAINT `consumer_credit_metrics_fk_credit_tag_id` FOREIGN KEY (credit_tag_id) REFERENCES credit_tag(id);
